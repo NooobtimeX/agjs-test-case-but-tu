d66b4edb79b35c80381cc65ee7afb95945c140b9
NooobtimeX <61542428+Nooobtime@users.noreply.github.com>
2023-04-09 14:19:19 +0700

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,7 +1,8 @@
 describe("cs262 assignment2", function () {
   it("test 1", async function () {
-    user="consult"
-    pass="consuzlt"
+    user = "consult";
+    pass = "consult";
+    var refNO = "1214334214";
     const EC = protractor.ExpectedConditions;
     await browser.get("http://twms.twss.co.th:8080/twms-dashboard/#/dashboard");
     await browser.driver.manage().window().maximize();
@@ -29,10 +30,15 @@ describe("cs262 assignment2", function () {
       .first();
     await browser.wait(EC.visibilityOf(salesOrder), 10000);
     await salesOrder.click();
-    //2.click SO and 3.
+    //2.click SO and
     var saleorderbtn = element(by.id("i_create_item"));
     await browser.wait(EC.visibilityOf(saleorderbtn), 10000);
     await saleorderbtn.click();
+    //3.
+    //test1
+    var check = element(by.xpath('//*[@id="tabstrip-2"]/form/div[1]/div/span/button[3]'));
+    await browser.wait(EC.visibilityOf(check), 10000);
+    expect(check.getText()).toEqual("Save&Confirm");
     //test 2.1
     expect(element(by.id("i_customer_insert")).isEnabled()).toBe(false);
     //4.a
@@ -52,7 +58,7 @@ describe("cs262 assignment2", function () {
         by.xpath('//*[@id="tabstrip-2"]/form/div[5]/div[1]/div/span[1]')
       ).getAttribute("aria-disabled")
     ).toBe("true");
-    //test 4.1
+    //test 4.1 be false because this enable after client not a customer
     expect(
       await element(
         by.xpath('//*[@id="tabstrip-2"]/form/div[3]/div[2]/div/span')
@@ -100,7 +106,6 @@ describe("cs262 assignment2", function () {
     await browser.wait(EC.visibilityOf(document_date), 10000);
     await document_date.sendKeys("01/04/2023");
     //4.g h i Ref Doc No
-    var refNO = 1214334214;
     var ref = element(by.id("i_ref_doc_insert"));
     await browser.wait(EC.visibilityOf(ref), 10000);
     await ref.sendKeys(refNO);
@@ -156,10 +161,9 @@ describe("cs262 assignment2", function () {
     );
     await browser.wait(EC.visibilityOf(searchbtn), 10000);
     await searchbtn.click();
+        //testcase 5
     var howmany = element(by.xpath('//*[@id="itemGridOptions"]/div[4]/span'));
     await browser.wait(EC.visibilityOf(howmany), 10000);
-    
-    //testcase 5
     expect(howmany.getText()).toEqual("1 - 1 of 1 items");
     var s_no_bpartner = element(by.id("s_no_bpartner"));
     await browser.wait(EC.visibilityOf(s_no_bpartner), 10000);
@@ -171,10 +175,29 @@ describe("cs262 assignment2", function () {
     expect(element(by.id("shiptosearch")).isEnabled()).toBe(true);
     expect(s_no_bpartner.isEnabled()).toBe(true);
     await browser.refresh();
+    //test8
     //input Ref Doc No.
+    var ref = element(by.id("i_ref_doc_number"));
+    await browser.wait(EC.visibilityOf(ref), 10000);
+    await ref.sendKeys(refNO);
     //Status slect to INITIAL
+    var Statusselect = element(
+      by.xpath('//*[@id="tabstrip-1"]/form/div[2]/div[4]/span')
+    );
+    await browser.wait(EC.visibilityOf(Statusselect), 10000);
+    await Statusselect.click();
+    var Statusoption = element(by.xpath('//*[@id="i_status_listbox"]/li[4]'));
+    await browser.wait(EC.visibilityOf(Statusoption), 10000);
+    await Statusoption.click();
     //clcik search
-    //testcase 8
+    var search = element(by.xpath('//*[@id="tabstrip-1"]/form/div[5]/button'));
+    await browser.wait(EC.visibilityOf(search), 10000);
+    await search.click();
+    expect(
+      element(
+        by.xpath('//*[@id="itemGridOptions"]/div[3]/table/tbody/tr[1]/td[6]')
+      ).getText()
+    ).toBe(refNO);
     //9 logout
     var userprofile = element(by.css(".user-profile.dropdown-toggle"));
     await browser.wait(EC.visibilityOf(userprofile), 10000);
