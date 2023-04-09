62c737f24047d6dbd259a02f0acd27e5b773130d
NooobtimeX <61542428+Nooobtime@users.noreply.github.com>
2023-04-09 14:41:26 +0700

    ss
    s

--- conf.js
+++ conf.js
@@ -1,11 +1,10 @@
 exports.config = {
-  // Capabilities to be passed to the webdriver instance.
   capabilities: {
     browserName: "chrome",
   },
-  seleniumAddress: "http://localhost:4444/wd/hub", // Framework to use: Jasmine
-  framework: "jasmine", // Spec Files relative to the current working directory contain test cases that protractor will run for testing.
-  specs: ["spec.js"], // Options to be passed to Jasmine.
+  seleniumAddress: "http://localhost:4444/wd/hub",
+  framework: "jasmine",
+  specs: ["spec.js"],
   jasmineNodeOpts: {
     defaultTimeoutInterval: 20000,
   },
