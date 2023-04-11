b7596bb53e54e9afb6e2ee3ff198add14cfcc4bb
CarToonCH9 <127950859+CarToonCH9@users.noreply.github.com>
2023-04-11 20:05:18 +0700

    Add files via upload

--- spec.js
+++ spec.js
@@ -18,9 +18,9 @@ describe("cs262 assignment2", function () {
     await browser.wait(EC.visibilityOf(loginbtn), 10000);
     await username.sendKeys(user);
     await password.sendKeys(pass);
-    await browser.sleep(0);
+    await browser.sleep(750);
     await loginbtn.click();
-    await browser.sleep(0);
+    await browser.sleep(750);
   });
 
   it("Create Sales Order", async () => {
@@ -31,7 +31,7 @@ describe("cs262 assignment2", function () {
     const year = time.getFullYear().toString();
 
     //define
-    var refNO = "test-createOrder-6409650089-020";
+    var refNO = "test-createOrder-640965XXXX-00Y";
     var customerid = "10009670";
     var productcodeid = "1000070";
     var qtys = "3";
@@ -45,7 +45,7 @@ describe("cs262 assignment2", function () {
       .then(function () {
         console.log("Login success");
       });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 1.1 : Click Outbound
     var outbound = element(by.cssContainingText("a", "Outbound"));
@@ -53,7 +53,7 @@ describe("cs262 assignment2", function () {
     await outbound.click().then(function () {
       console.log("Click Outbound");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 1.2 : Click Sales Orders
     var salesOrder = element.all(by.cssContainingText("a", "Sales Orders"));
@@ -64,7 +64,7 @@ describe("cs262 assignment2", function () {
       .then(function () {
         console.log("Click Sales Orders");
       });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 2 : Click create SO
     var saleorderbtn = element(by.id("i_create_item"));
@@ -72,7 +72,7 @@ describe("cs262 assignment2", function () {
     await saleorderbtn.click().then(function () {
       console.log("Click create SO");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 3 : The system displays a new sales order form.
 
@@ -80,11 +80,11 @@ describe("cs262 assignment2", function () {
     await browser.wait(EC.visibilityOf(check), 10000).then(function () {
       console.log("show sale order form");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 2.1 : Client's Customer should disable
     expect(element(by.id("i_customer_insert")).isEnabled()).toBe(false);
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 4.a : Select client to open SO
     var clientslect = element(by.css('[aria-owns="i_client_listbox"]'));
@@ -95,11 +95,11 @@ describe("cs262 assignment2", function () {
     await clientoptioon.click().then(function () {
       console.log("Select client");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 2.2 : Client's Customer should enable
     expect(element(by.id("i_customer_insert")).isEnabled()).toBe(true);
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 3.1 : Client's Ship To Address should disable
     expect(
@@ -107,7 +107,7 @@ describe("cs262 assignment2", function () {
         by.css('[aria-owns="i_shipto_address_insert_listbox"]')
       ).getAttribute("aria-disabled")
     ).toBe("true");
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 4.1 : Client's Shipping Method should disable
     //?????
@@ -116,7 +116,7 @@ describe("cs262 assignment2", function () {
         by.css('[aria-owns="i_shiping_method_insert_listbox"]')
       ).getAttribute("aria-disabled")
     ).toBe("true");
-    await browser.sleep(0);
+    await browser.sleep(750);
     //should have 1 false because of wrong functional req
 
     //Basic Flow 4.b : Input customer data
@@ -128,7 +128,7 @@ describe("cs262 assignment2", function () {
     await customeroption.click().then(function () {
       console.log("Input customer data");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 3.2 : Client's Ship To Address should enable
     expect(
@@ -136,7 +136,7 @@ describe("cs262 assignment2", function () {
         by.css('[aria-owns="i_shipto_address_insert_listbox"]')
       ).getAttribute("aria-disabled")
     ).toBe("false");
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 4.2 : Client's Shipping Method should enable
     expect(
@@ -144,7 +144,7 @@ describe("cs262 assignment2", function () {
         by.css('[aria-owns="i_shiping_method_insert_listbox"]')
       ).getAttribute("aria-disabled")
     ).toBe("false");
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 4.c : Select Ship To Address to open SO
     var staselect = element(
@@ -159,7 +159,7 @@ describe("cs262 assignment2", function () {
     await staoption.click().then(function () {
       console.log("Select Ship To Address to open SO");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 4.e Input Request Date (no 4.d)
     var request_date_insert = element(by.id("i_request_date_insert"));
@@ -167,7 +167,7 @@ describe("cs262 assignment2", function () {
     await request_date_insert.sendKeys(currentDate).then(function () {
       console.log("Input Request Date");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 4.f Input So Date
     var document_date = element(by.id("i_document_date"));
@@ -175,7 +175,7 @@ describe("cs262 assignment2", function () {
     await document_date.sendKeys(currentDate).then(function () {
       console.log("Input So Date");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 4.g h i : Input Ref Doc No
     var ref = element(by.id("i_ref_doc_insert"));
@@ -183,7 +183,7 @@ describe("cs262 assignment2", function () {
     await ref.sendKeys(refNO).then(function () {
       console.log("Input Ref Doc No");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 5 : Click create line item
     var createLineItemButton = element(by.id("i_create_line_item"));
@@ -191,7 +191,7 @@ describe("cs262 assignment2", function () {
     await createLineItemButton.click().then(function () {
       console.log("Click create line item");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 5.a : Input productcode
     var productcode = element(by.name("productcode"));
@@ -204,7 +204,7 @@ describe("cs262 assignment2", function () {
     await productoption.click().then(function () {
       console.log("Input productcode");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 5.b : Input Qty
     var qty = element(
@@ -216,7 +216,7 @@ describe("cs262 assignment2", function () {
     await qty.sendKeys(qtys).then(function () {
       console.log("Input Qty");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 5.c : Select SU
     var seselect = element(
@@ -229,7 +229,7 @@ describe("cs262 assignment2", function () {
     await seoption.click().then(function () {
       console.log("Select SU");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 6 : Click Update button
     var updatebtn = element(by.cssContainingText("a", "Update"));
@@ -237,7 +237,7 @@ describe("cs262 assignment2", function () {
     await updatebtn.click().then(function () {
       console.log("Click Update button");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 7 : Click Save&Confirm
     var saveconfirm = element(by.cssContainingText("button", "Save&Confirm"));
@@ -245,7 +245,7 @@ describe("cs262 assignment2", function () {
     await saveconfirm.click().then(function () {
       console.log("Click Save&Confirm");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Basic Flow 8 : Click OK from alert box
     await browser.wait(EC.alertIsPresent(), 5000);
@@ -253,7 +253,7 @@ describe("cs262 assignment2", function () {
     await browser.refresh().then(function () {
       console.log("Click ok from alert box and refresh");
     });
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 5 : The system check SO number auto generate unique number
     var i_ref_doc_number = element(by.id("i_ref_doc_number"));
@@ -267,7 +267,7 @@ describe("cs262 assignment2", function () {
     await searchbtn.click();
     await browser.wait(EC.visibilityOf(howmany), 10000);
     expect(howmany.getText()).toEqual("1 - 1 of 1 items");
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 6 : Product displayed must belong to that client only.
     var refs = element(
@@ -285,7 +285,7 @@ describe("cs262 assignment2", function () {
     var checkbox = element(by.id("chkSearchAllRow"));
     await browser.wait(EC.visibilityOf(checkbox), 10000);
     expect(checkbox.isEnabled()).toBe(true);
-    await browser.sleep(0);
+    await browser.sleep(750);
 
     //Test Case 8 : Verify customer as a bpartner exclusive to this client.
     var s_no_bpartner = element(by.id("s_no_bpartner"));
@@ -293,7 +293,7 @@ describe("cs262 assignment2", function () {
     await s_no_bpartner.click();
     await searchbtn.click();
     expect(howmany.getText()).toEqual("No items to display");
-    await browser.sleep(0);
+    await browser.sleep(750);
   });
 
   afterEach(async () => {
